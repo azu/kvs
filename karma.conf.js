9fd2b24e66b9cdd23f8b725684fd8876f446ce7f
azu <azuciao@gmail.com>
2020-08-07 21:58:57 +0900

    CI: debug SAUCE_USERNAME

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+console.log("SAUCE_USERNAME", process.env.SAUCE_USERNAME);
 module.exports = require("@jsdevtools/karma-config")({
     tests: ["src/**/*.ts", "test/**/*.test.ts"],
     browsers: Boolean(process.env.CI) ? {} : { safari: false },
