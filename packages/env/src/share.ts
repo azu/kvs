ee6dadacbb1d2e003956162b6cb7a5611f99a391
azu <azuciao@gmail.com>
2020-08-09 02:07:00 +0900

    test(env): fix test

--- packages/env/src/share.ts
+++ packages/env/src/share.ts
@@ -1,8 +1,8 @@
 import { JsonValue } from "@kvs/storage";
-import { KVS } from "@kvs/types";
+import { KVS, KVSOptions } from "@kvs/types";
 
 export type KvsEnvStorageSchema = {
     [index: string]: JsonValue;
 };
 export type KvsEnvStorage<Schema extends KvsEnvStorageSchema> = KVS<Schema>;
-export type KvsEnvStorageOptions<Schema extends KvsEnvStorageSchema> = KvsEnvStorage<Schema>;
+export type KvsEnvStorageOptions<Schema extends KvsEnvStorageSchema> = KVSOptions<Schema>;
