9c3dc6d6268db7dedbbcaa4891a073cd421666a6
azu <azuciao@gmail.com>
2020-08-12 00:27:31 +0900

    fix: test on pr

--- packages/common-test-case/src/common-test-case.ts
+++ packages/common-test-case/src/common-test-case.ts
@@ -111,16 +111,15 @@ export const createKVSTestCase = (
                 );
             });
             it("does not upgrade when open first version", async () => {
-                let isupgradeCalled = false;
+                let isUpgradeCalled = false;
                 kvs = ref.current = await kvsStorageConstructor({
                     version: 1,
                     async upgrade() {
-                        isupgradeCalled = true;
+                        isUpgradeCalled = true;
                     }
                 });
                 await kvs.set("key1", "value1");
-                await kvs.close();
-                assert.strictEqual(isupgradeCalled, false, "should not call upgrade at first time");
+                assert.strictEqual(isUpgradeCalled, false, "should not call upgrade at first time");
             });
             it("upgrade when on upgrade version", async () => {
                 kvs = ref.current = await kvsStorageConstructor({
